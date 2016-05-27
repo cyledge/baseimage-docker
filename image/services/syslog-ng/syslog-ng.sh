519f57b6361d7e92cb5178234848d1d7cefc4acb
Günter Dressel <g.dressel@cyledge.com>
2016-05-27 14:56:37 +0200

    made syslog-ng.conf "release aware" by dynamically modifying it during build process

--- image/services/syslog-ng/syslog-ng.sh
+++ image/services/syslog-ng/syslog-ng.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 . /build/buildconfig
+. /etc/lsb-release
 
 SYSLOG_NG_BUILD_PATH=/build/services/syslog-ng
 
@@ -14,11 +15,20 @@ cp $SYSLOG_NG_BUILD_PATH/syslog-ng.conf /etc/syslog-ng/syslog-ng.conf
 #
 # In Ubuntu 12.04 no syslog user gets created by APT/dpkg
 # Let's create it manually
-user_exists=$(id syslog &> /dev/null; echo $?)
-if [ $user_exists != 0 ]
+#
+if [ $DISTRIB_RELEASE == "12.04" ]
 then
   groupadd -r -g 104 syslog
   useradd -r -M -l -u 101 -g 104 -s /bin/false syslog 
+fi
+
+#
+# Syslog-ng version of Ubuntu 12.04 requires older config file version.
+#
+if [ $DISTRIB_RELEASE == "12.04" ]
+then
+  echo "downgrading syslog-ng.conf version header to 3.3 (required for Ubuntu 12.04)"
+  sed "s/^@version: [[:digit:]].[[:digit:]]/@version: 3.3/" -i /etc/syslog-ng/syslog-ng.conf
 fi
 
 chown syslog:syslog /var/lib/syslog-ng
