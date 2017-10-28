86665ce39983567158c1bd84be1c4f4077b120e2
Günter Dressel <g.dressel@cyledge.com>
2017-10-28 08:57:49 +0200

    Changed switch mechanic between syslog-ng log targets

--- image/services/syslog-ng/install.sh
+++ image/services/syslog-ng/install.sh
@@ -13,10 +13,13 @@ apt_install syslog-ng-core
 mkdir /etc/service/syslog-ng
 cp $SYSLOG_NG_BUILD_PATH/run.sh /etc/service/syslog-ng/run
 cp $SYSLOG_NG_BUILD_PATH/check.sh /etc/my_init.d/check-syslog-ng.sh
-cp $SYSLOG_NG_BUILD_PATH/syslog-ng.conf /etc/syslog-ng/
-cp $SYSLOG_NG_BUILD_PATH/log-fluent.conf /etc/syslog-ng/
-cp $SYSLOG_NG_BUILD_PATH/log-stdout.conf /etc/syslog-ng/
 
+mkdir /etc/syslog-ng/conf-stdout.d
+mkdir /etc/syslog-ng/conf-fluent.d
+
+cp $SYSLOG_NG_BUILD_PATH/syslog-ng.conf /etc/syslog-ng/
+cp $SYSLOG_NG_BUILD_PATH/log-fluent.conf /etc/syslog-ng/conf-fluent.d/basic.conf
+cp $SYSLOG_NG_BUILD_PATH/log-stdout.conf /etc/syslog-ng/conf-stdout.d/basic.conf
 
 #
 # Create user syslog manually if not present
