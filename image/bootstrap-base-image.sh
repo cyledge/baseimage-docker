b9eb751c35a6a85d189a8926f3f3fb47a7b19fdc
Günter Dressel <g.dressel@cyledge.com>
2017-11-01 12:29:10 +0100

    Allow every user to source /etc/container_environment.sh in bash.rc

--- image/bootstrap-base-image.sh
+++ image/bootstrap-base-image.sh
@@ -88,7 +88,7 @@ mkdir -p /etc/my_init.d
 mkdir -p /etc/container_environment
 touch /etc/container_environment.sh
 touch /etc/container_environment.json
-chmod 700 /etc/container_environment
+chmod 755 /etc/container_environment
 
 groupadd -g 8377 docker_env
 chown :docker_env /etc/container_environment.sh /etc/container_environment.json
