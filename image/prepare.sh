161612df63b4d664609bd70a6ffe46fa975ea1eb
guedressel <g.dressel@cyledge.com>
2019-10-22 11:21:53 +0200

    Update prepare.sh: adopt changes from phusion/basimage-docker

--- image/prepare.sh
+++ image/prepare.sh
@@ -23,9 +23,23 @@ set -e
 . /etc/lsb-release
 
 
-## Enable Ubuntu Universe and Multiverse.
+## Enable Ubuntu Universe, Multiverse, and deb-src for main.
+sed -i 's/^#\s*\(deb.*main restricted\)$/\1/g' /etc/apt/sources.list
 sed -i 's/^#\s*\(deb.*universe\)$/\1/g' /etc/apt/sources.list
 sed -i 's/^#\s*\(deb.*multiverse\)$/\1/g' /etc/apt/sources.list
+
+
+## Fix some issues with APT packages.
+## See https://github.com/dotcloud/docker/issues/1024
+dpkg-divert --local --rename --add /sbin/initctl
+ln -sf /bin/true /sbin/initctl
+
+## Replace the 'ischroot' tool to make it always return true.
+## Prevent initscripts updates from breaking /dev/shm.
+## https://journal.paul.querna.org/articles/2013/10/15/docker-ubuntu-on-rackspace/
+## https://bugs.launchpad.net/launchpad/+bug/974584
+dpkg-divert --local --rename --add /usr/bin/ischroot
+ln -sf /bin/true /usr/bin/ischroot
 
 
 if [ "$IMAGE_BUILD_DEBUG" -eq 0 ]; then
@@ -43,6 +57,17 @@ else
 fi
 
 
+status "Install apt tools..."
+## Install HTTPS support for APT.
+apt_install apt-transport-https ca-certificates software-properties-common
+
+
+## Fix locale.
+apt_install language-pack-en
+locale-gen en_US
+update-locale LANG=en_US.UTF-8 LC_CTYPE=en_US.UTF-8
+echo -n en_US.UTF-8 > /etc/container_environment/LANG
+echo -n en_US.UTF-8 > /etc/container_environment/LC_CTYPE
 
 
 ## Install a syslog daemon and logrotate.
