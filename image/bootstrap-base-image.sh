c74927f9cef4032a91ef0ff15ec39f52b9161463
Günter Dressel <g.dressel@cyledge.com>
2022-06-24 12:50:01 +0200

    Fixed 18.04 compatibility

--- image/bootstrap-base-image.sh
+++ image/bootstrap-base-image.sh
@@ -9,14 +9,16 @@ status "Tweaking environment..."
 
 ## Fix some issues with APT packages.
 ## See https://github.com/dotcloud/docker/issues/1024
-dpkg-divert --local --rename --add /sbin/initctl
+[ $DISTRIB_RELEASE == "18.04" ] && dpkg-divert --local --add /sbin/initctl || true
+[ $DISTRIB_RELEASE == "18.04" ] || dpkg-divert --local --no-rename --add /sbin/initctl
 ln -sf /bin/true /sbin/initctl
 
 ## Replace the 'ischroot' tool to make it always return true.
 ## Prevent initscripts updates from breaking /dev/shm.
 ## https://journal.paul.querna.org/articles/2013/10/15/docker-ubuntu-on-rackspace/
 ## https://bugs.launchpad.net/launchpad/+bug/974584
-dpkg-divert --local --no-rename --add /usr/bin/ischroot
+[ $DISTRIB_RELEASE == "18.04" ] && dpkg-divert --local --add /usr/bin/ischroot || true
+[ $DISTRIB_RELEASE == "18.04" ] || dpkg-divert --local --no-rename --add /usr/bin/ischroot
 ln -sf /bin/true /usr/bin/ischroot
 
 
