694c24d9b41b8bb976b37ec4eda268eda2aa1d8b
Günter Dressel <g.dressel@cyledge.com>
2022-06-27 19:43:54 +0200

    #10 implemented vacuum-tmp

--- image/services/cron/install.sh
+++ image/services/cron/install.sh
@@ -5,15 +5,21 @@ set -e
 
 status "Installing cron..."
 
-apt_install cron
+apt_install cron bc
 mkdir /etc/service/cron
 chmod 600 /etc/crontab
 cp /build/services/cron/run.sh /etc/service/cron/run
+
 
 ## Remove useless cron entries.
 # Checks for lost+found and scans for mtab.
 rm -f /etc/cron.daily/standard
 rm -f /etc/cron.daily/upstart
 rm -f /etc/cron.daily/dpkg
+rm -f /etc/cron.daily/apt-compat
 rm -f /etc/cron.daily/password
 rm -f /etc/cron.weekly/fstrim
+
+
+# install vacuum-tmp cronjob
+cp /build/bin/vacuum-tmp /etc/cron.hourly/vacuum-tmp
